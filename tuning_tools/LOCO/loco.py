619bbef66d4fec593aec63928485c2694f61adb1
PONS <pons@esrf.fr>
2025-06-23 14:40:04 +0200

    Moved tunning tools

--- tuning_tools/LOCO/loco.py
+++
@@ -1,8 +0,0 @@
-class loco():
-    """loco help"""
-
-    def run():
-        """loco run help"""
-
-    def measure_orm():
-        """loco measure help"""
--- tuning_tools/SOFB/sofb.py
+++
@@ -1,8 +0,0 @@
-class slow_orbit_feeback():
-    """slow orbit feedback help"""
-
-    def run():
-        """run slow orbit feedback"""
-
-    def get_svd_decomposition():
-        """get steerers svd decomposition"""
