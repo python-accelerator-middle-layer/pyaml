4d25ae7c6daad5f87ca32349798916ee2225093e
PONS <pons@esrf.fr>
2025-06-28 08:53:55 +0200

    Cometic changes for external module example

--- tests/external/pyaml_external/ExternalUnitConv.py
+++ tests/external/pyaml_external/ExternalUnitConv.py
@@ -4,28 +4,30 @@ from pyaml.configuration.Factory import validate
 from pyaml.control.Device import Device
 
 """
-Class that handle manget current/strength conversion using linear interpolation for a single function magnet
+Class example proving manget current/strength conversion
 """
 class ExternalUnitConv(UnitConv):
 
     @validate
-    def __init__(self, powersupply:Device, param: str = None):
-        print("ExternalUnitConv: Device + " + powersupply.setpoint + ", Param:" + param)
+    def __init__(self, powersupply:Device, id:Device, param: str = None):
+        print("ExternalUnitConv:\n  powersupply:%s\n  id:%s\n  param:%s" 
+              % (powersupply.setpoint,id.setpoint,param))
 
     # Get coil current(s) from magnet strength(s)
     def get_currents(self,strengths:np.array) -> np.array:
-        print("In get_currents")
+        print("ExternalUnitConv.get_currents(%f)"%strengths[0])
         return np.array([strengths[0]*1.5])
 
     # Get magnet strength(s) from coil current(s)
-    def get_strengths(self,strengths:np.array) -> np.array:
-        print("In get_strengths")
+    def get_strengths(self,currents:np.array) -> np.array:
+        print("ExternalUnitConv.get_strengths(%f)"%currents[0])
+        return np.array([currents[0]/1.5])
 
     # Set magnet rigidity
     def set_magnet_rigidity(self,brho:np.double):
         pass
 
 def factory_constructor(config: dict) -> ExternalUnitConv:
-   """Construct a Linear unit conversion object from Yaml config file"""
+   """Construct an ExternalUnitConv object from Yaml config file"""
    return ExternalUnitConv(**config)
 
