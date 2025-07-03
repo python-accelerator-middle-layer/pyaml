313573a221511d80e3722029e77ceea7ba44be11
PONS <pons@esrf.fr>
2025-07-03 06:19:12 +0200

    Fix for combined function magnet

--- pyaml/magnet/linear_cfm_unitconv.py
+++ pyaml/magnet/linear_cfm_unitconv.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 
 import numpy as np
 from ..configuration.curve import Curve
-from ..configuration.csvcurve import CSVCurve
 from ..configuration.matrix import Matrix
 from ..control.deviceaccess import DeviceAccess
 from .unitconv import UnitConv
@@ -10,34 +9,44 @@ from .unitconv import UnitConv
 # Define the main class name for this module
 PYAMLCLASS = "LinearCFMagnetUnitConv"
 
-class ConfigModel(BaseModel):
 
+class ConfigModel(BaseModel):
     multipoles: list[str]
-    """List of supported functions, A0,B0,B0,B1,etc (i.e) [B0,A1,B2]"""
+    """List of supported functions: A0,B0,B0,B1,etc (i.e) [B0,A1,B2]"""
     curves: list[Curve]
-    """Calibration curves used for interpolation, 1 curve per function"""
+    """Exitacion curves, 1 curve per function"""
     calibration_factors: list[float] = None
-    """Correction factor applied to curves, 1 factor per function"""
+    """Correction factor applied to curves, 1 factor per function
+       Delfault: ones"""
     calibration_offsets: list[float] = None
-    """Correction offset applied to curves, 1 offset per function"""
+    """Correction offset applied to curves, 1 offset per function
+       Delfault: zeros"""
+    pseudo_factors: list[float] = None
+    """Factors applied to 'pseudo currents', 1 sign per function.
+       Delfault: ones"""
     powerconverters: list[DeviceAccess]
-    """List of power converter devices to apply currrents (can be different from number of function)"""
-    matrix: Matrix
-    """n x m matrix (n rows for n function , m columns for m currents) to handle cross depecdancies between functions"""
+    """List of power converter devices to apply currrents (can be different
+       from number of function)"""
+    matrix: Matrix = None
+    """n x m matrix (n rows for n function , m columns for m currents) 
+       to handle multipoles separation. Default: Identity"""
     units: list[str]
     """List of strength unit (i.e. ['rad','m-1','m-2'])"""
 
 
 class LinearCFMagnetUnitConv(UnitConv):
     """
-    Class providing a simple linear model for combined function magnets
+    Class providing a simple linear model for combined function magnets. A matrix
+    can handle separation of multipoles. A pseudo current is a linear combination
+    of power supply currents assiociated to a single function.
     """
-    def __init__(self, cfg:ConfigModel):
+
+    def __init__(self, cfg: ConfigModel):
         self._cfg = cfg
 
         # Check config
-        self._nbFunction:int = len(cfg.multipoles)
-        self._nbPS:int = len(cfg.powerconverters)
+        self._nbFunction: int = len(cfg.multipoles)
+        self._nbPS: int = len(cfg.powerconverters)
 
         if cfg.calibration_factors is None:
             self._calibration_factors = np.ones(self._nbFunction)
@@ -49,30 +58,52 @@ class LinearCFMagnetUnitConv(UnitConv):
         else:
             self._calibration_offsets = cfg.calibration_offsets
 
+        if cfg.pseudo_factors is None:
+            self._pf = np.ones(self._nbFunction)
+        else:
+            self._pf = cfg.pseudo_factors
+
         if len(self._calibration_factors) != self._nbFunction:
-            raise Exception("calibration_factors does not have the expected " \
-            "number of items ({self._nbFunction} expected)")
+            raise Exception(
+                "calibration_factors does not have the expected "
+                "number of items ({self._nbFunction} expected)"
+            )
         if len(self._calibration_offsets) != self._nbFunction:
-            raise Exception("calibration_offsets does not have the expected " \
-            "number of items ({self._nbFunction} expected)")
+            raise Exception(
+                "calibration_offsets does not have the expected "
+                "number of items ({self._nbFunction} expected)"
+            )
+        if len(self._pf) != self._nbFunction:
+            raise Exception(
+                "pseudo_factors does not have the expected "
+                "number of items ({self._nbFunction} expected)"
+            )
         if len(cfg.units) != self._nbFunction:
-            raise Exception("units does not have the expected " \
-            "number of items ({self._nbFunction} expected)")
+            raise Exception(
+                "units does not have the expected "
+                "number of items ({self._nbFunction} expected)"
+            )
         if len(cfg.curves) != self._nbFunction:
-            raise Exception("curves does not have the expected " \
-            "number of items ({self._nbFunction} expected)")
-        
-        self._matrix = cfg.matrix.get_matrix()
+            raise Exception(
+                "curves does not have the expected "
+                "number of items ({self._nbFunction} expected)"
+            )
+
+        if cfg.matrix is None:
+            self._matrix = np.identity(self._nbFunction)
+        else:
+            self._matrix = cfg.matrix.get_matrix()
 
         _s = np.shape(self._matrix)
 
         if len(_s) != 2 or _s[0] != self._nbFunction or _s[1] != self._nbPS:
-            raise Exception("matrix wrong dimension "
-            "({self._nbFunction}x{self._nbPS} expected but got {_s[0]}x{_s[1]})")
-        
+            raise Exception(
+                "matrix wrong dimension "
+                "({self._nbFunction}x{self._nbPS} expected but got {_s[0]}x{_s[1]})"
+            )
 
         # Apply factor and offset
-        for idx,c in enumerate(cfg.curves):         
+        for idx, c in enumerate(cfg.curves):
             c.get_curve()[:, 1] *= self._calibration_factors[idx]
             c.get_curve()[:, 1] += self._calibration_offsets[idx]
 
@@ -80,19 +111,26 @@ class LinearCFMagnetUnitConv(UnitConv):
         self._inv = np.linalg.pinv(self._matrix)
 
     # Get coil current(s) from magnet strength(s)
-    def compute_currents(self,strengths:np.array) -> np.array:
+    def compute_currents(self, strengths: np.array) -> np.array:
         _pI = np.zeros(self._nbFunction)
-        for idx,c in enumerate(self._cfg.curves):
-            _pI[idx] = np.interp(strengths[idx], c[:, 1], c[:, 0]) * self._brho
-        _currents = np.matmul(self._inv,_pI)
+        for idx, c in enumerate(self._cfg.curves):
+            _pI[idx] = self._pf[idx] * np.interp(
+                strengths[idx] * self._brho, c.get_curve()[:, 1], c.get_curve()[:, 0]
+            )
+        _currents = np.matmul(self._inv, _pI)
         return _currents
 
     # Get magnet strength(s) from coil current(s)
-    def compute_strengths(self,currents:np.array) -> np.array:
+    def compute_strengths(self, currents: np.array) -> np.array:
         _strength = np.zeros(self._nbFunction)
-        _pI = np.matmul(self._matrix,currents)
-        for idx,c in enumerate(self._cfg.curves):
-            _strength[idx] = np.interp(_pI[idx], c[:, 0], c[:, 1]) / self._brho
+        _pI = np.matmul(self._matrix, currents)
+        for idx, c in enumerate(self._cfg.curves):
+            _strength[idx] = (
+                np.interp(
+                    self._pf[idx] * _pI[idx], c.get_curve()[:, 0], c.get_curve()[:, 1]
+                )
+                / self._brho
+            )
         return _strength
 
     # Get strength units
@@ -121,12 +159,10 @@ class LinearCFMagnetUnitConv(UnitConv):
         return np.array(_c)
 
     # Send power supply current(s) to control system
-    def send_currents(self,currents:np.array):
-        for idx,p in enumerate(self._cfg.powerconverters):
+    def send_currents(self, currents: np.array):
+        for idx, p in enumerate(self._cfg.powerconverters):
             p.set(currents[idx])
 
     # Set magnet rigidity
-    def set_magnet_rigidity(self,brho:np.double):
+    def set_magnet_rigidity(self, brho: np.double):
         self._brho = brho
-
-
