a938b0c3b9dafb4ed9decdf14aa6efdb518fe949
PONS <pons@esrf.fr>
2025-07-28 13:06:41 +0200

    Updated private fields

--- pyaml/magnet/linear_model.py
+++ pyaml/magnet/linear_model.py
@@ -28,48 +28,48 @@ class LinearMagnetModel(MagnetModel):
 
     def __init__(self, cfg: ConfigModel):
         self._cfg = cfg
-        self._curve = cfg.curve.get_curve()
-        self._curve[:, 1] = (
-            self._curve[:, 1] * cfg.calibration_factor + cfg.calibration_offset
+        self.__curve = cfg.curve.get_curve()
+        self.__curve[:, 1] = (
+            self.__curve[:, 1] * cfg.calibration_factor + cfg.calibration_offset
         )
-        self._strength_unit = cfg.unit
-        self._current_unit = cfg.powerconverter.unit()
-        self._brho = np.nan
-        self._ps = cfg.powerconverter
+        self.__strength_unit = cfg.unit
+        self.__hardware_unit = cfg.powerconverter.unit()
+        self.__brho = np.nan
+        self.__ps = cfg.powerconverter
 
     def compute_hardware_values(self, strengths: np.array) -> np.array:
         _current = np.interp(
-            strengths[0] * self._brho, self._curve[:, 1], self._curve[:, 0]
+            strengths[0] * self.__brho, self.__curve[:, 1], self.__curve[:, 0]
         )
         return np.array([_current])
 
     def compute_strengths(self, currents: np.array) -> np.array:
         _strength = (
-            np.interp(currents[0], self._curve[:, 0], self._curve[:, 1]) / self._brho
+            np.interp(currents[0], self.__curve[:, 0], self.__curve[:, 1]) / self.__brho
         )
         return np.array([_strength])
 
     def get_strength_units(self) -> list[str]:
-        return [self._strength_unit] if self._strength_unit is not None else [""]
+        return [self.__strength_unit] if self.__strength_unit is not None else [""]
 
     def get_hardware_units(self) -> list[str]:
-        return [self._current_unit] if self._current_unit is not None else [""]
+        return [self.__hardware_unit] if self.__hardware_unit is not None else [""]
 
     def read_hardware_values(self) -> np.array:
-        return [self._ps.get()]
+        return [self.__ps.get()]
 
     def readback_hardware_values(self) -> np.array:
-        return [self._ps.readback()]
+        return [self.__ps.readback()]
 
     def send_harware_values(self, currents: np.array):
-        self._ps.set(currents[0])
+        self.__ps.set(currents[0])
 
     def set_magnet_rigidity(self, brho: np.double):
-        self._brho = brho
+        self.__brho = brho
 
     def __repr__(self):
         return "%s(curve[%d pts], unit=%s)" % (
             self.__class__.__name__,
-            len(self._curve),
-            self._strength_unit,
+            len(self.__curve),
+            self.__strength_unit,
         )
--- pyaml/magnet/spline_model.py
+++ pyaml/magnet/spline_model.py
@@ -31,46 +31,46 @@ class SplineMagnetModel(MagnetModel):
 
     def __init__(self, cfg: ConfigModel):
         self._cfg = cfg
-        self._curve = cfg.curve.get_curve()
-        self._curve[:, 1] = (
-            self._curve[:, 1] * cfg.calibration_factor + cfg.calibration_offset
+        self.__curve = cfg.curve.get_curve()
+        self.__curve[:, 1] = (
+            self.__curve[:, 1] * cfg.calibration_factor + cfg.calibration_offset
         )
-        self._strength_unit = cfg.unit
-        self._current_unit = cfg.powerconverter.unit()
-        self._brho = np.nan
-        self._ps = cfg.powerconverter
-        self._spl = make_smoothing_spline(self._curve[:, 0], self._curve[:, 1], lam=cfg.alpha)
-        self._rspl = make_smoothing_spline(self._curve[:, 1], self._curve[:, 0], lam=cfg.alpha)
+        self.__strength_unit = cfg.unit
+        self.__hardware_unit = cfg.powerconverter.unit()
+        self.__brho = np.nan
+        self.__ps = cfg.powerconverter
+        self.__spl = make_smoothing_spline(self.__curve[:, 0], self.__curve[:, 1], lam=cfg.alpha)
+        self.__rspl = make_smoothing_spline(self.__curve[:, 1], self.__curve[:, 0], lam=cfg.alpha)
 
     def compute_hardware_values(self, strengths: np.array) -> np.array:
-        _current = self._rspl(strengths[0] * self._brho)
+        _current = self.__rspl(strengths[0] * self.__brho)
         return np.array([_current])
 
     def compute_strengths(self, currents: np.array) -> np.array:
-        _strength = self._spl(currents[0]) / self._brho
+        _strength = self.__spl(currents[0]) / self.__brho
         return np.array([_strength])
 
     def get_strength_units(self) -> list[str]:
-        return [self._strength_unit] if self._strength_unit is not None else [""]
+        return [self.__strength_unit] if self.__strength_unit is not None else [""]
 
     def get_hardware_units(self) -> list[str]:
-        return [self._current_unit] if self._current_unit is not None else [""]
+        return [self.__hardware_unit] if self.__hardware_unit is not None else [""]
 
     def read_hardware_values(self) -> np.array:
-        return [self._ps.get()]
+        return [self.__ps.get()]
 
     def readback_hardware_values(self) -> np.array:
-        return [self._ps.readback()]
+        return [self.__ps.readback()]
 
     def send_harware_values(self, currents: np.array):
-        self._ps.set(currents[0])
+        self.__ps.set(currents[0])
 
     def set_magnet_rigidity(self, brho: np.double):
-        self._brho = brho
+        self.__brho = brho
 
     def __repr__(self):
         return "%s(curve[%d], unit=%s)" % (
             self.__class__.__name__,
-            len(self._curve),
-            self._strength_unit,
+            len(self.__curve),
+            self.__strength_unit,
         )
