f69cd054661735cb9ac06612a72b3b4a95c591b8
PONS <pons@esrf.fr>
2025-07-29 16:03:17 +0200

    Added fill_device() for ControlSystem

--- pyaml/control/controlsystem.py
+++ pyaml/control/controlsystem.py
@@ -1,10 +1,19 @@
 from abc import ABCMeta, abstractmethod
 from pydantic import BaseModel
+from ..lattice.element_holder import ElementHolder
+from ..lattice.element import Element
+from ..control.abstract_impl import RWHardwareScalar,RWHardwareArray,RWStrengthScalar,RWStrengthArray
+from ..magnet.magnet import Magnet
+from ..magnet.cfm_magnet import CombinedFunctionMagnet
 
-class ControlSystem(BaseModel,metaclass=ABCMeta):
+class ControlSystem(BaseModel,ElementHolder,metaclass=ABCMeta):
     """
     Abstract class providing access to a control system float variable
     """
+
+    def __init__(self):
+        BaseModel.__init__(self)
+        ElementHolder.__init__(self)
 
     @abstractmethod
     def init_cs(self):
@@ -15,3 +24,40 @@ class ControlSystem(BaseModel,metaclass=ABCMeta):
     def name(self) -> str:
         """Return control system name (i.e. live)"""
         pass
+
+    def set_energy(self,E:float):
+        """
+        Sets the energy on magnets belonging to this control system
+        
+        Parameters
+        ----------
+        E : float
+            Energy in eV
+        """
+        for m in self.__MAGNETS.items():
+            m[1].set_energy(E)
+    
+    def fill_device(self,elements:list[Element]):
+        """
+        Fill device of this control system with Element coming from the configuration file
+        
+        Parameters
+        ----------
+        elements : list[Element]
+            List of elements coming from the configuration file to attach to this control system
+        """           
+        for e in elements:
+          if isinstance(e,Magnet):
+            current = RWHardwareScalar(e.model)
+            strength = RWStrengthScalar(e.model)
+            # Create a unique ref for this control system
+            m = e.attach(strength,current)
+            self.add_magnet(str(m),m)
+          elif isinstance(e,CombinedFunctionMagnet):
+            self.add_magnet(str(e),e)
+            currents = RWHardwareArray(e.model)
+            strengths = RWStrengthArray(e.model)
+            # Create unique refs of each function for this control system
+            ms = e.attach(strengths,currents)
+            for m in ms:
+              self.add_magnet(str(m),m)
