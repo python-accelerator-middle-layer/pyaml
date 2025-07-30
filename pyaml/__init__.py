406a248ee7215dd954c5f76f2c783a5defd04e96
guillaumepichon <guillaume.pichon@synchrotron-soleil.fr>
2025-07-30 18:04:17 +0200

    First logging implementation for PyAML.

--- pyaml/__init__.py
+++ pyaml/__init__.py
@@ -12,7 +12,19 @@ __version__ = "0.0.0"
 __author__ = "pyAML collaboration"
 __author_email__ = ""
 
+import logging.config
+import os
 from pyaml.exception import PyAMLException
 
-__all__ = [__version__, "YamlLoader", PyAMLException]
+__all__ = [__version__, PyAMLException]
 
+
+config_file = os.getenv("PYAML_LOG_CONFIG", "pyaml_logging.conf")
+
+if os.path.exists(config_file):
+    logging.config.fileConfig(config_file, disable_existing_loggers=False)
+
+logger = logging.getLogger("pyaml")
+level = os.getenv("PYAML_LOG_LEVEL", "").upper()
+if len(level)>0:
+    logger.setLevel(getattr(logging, level, logging.WARNING))
