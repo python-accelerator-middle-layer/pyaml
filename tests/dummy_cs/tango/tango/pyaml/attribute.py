abdc25a75073618bffb7e34efede7a7e31970e0b
guillaumepichon <guillaume.pichon@synchrotron-soleil.fr>
2025-07-30 15:03:50 +0200

    Correction of merge errors.

--- tests/dummy_cs/tango/tango/pyaml/attribute.py
+++ tests/dummy_cs/tango/tango/pyaml/attribute.py
@@ -9,14 +9,10 @@ class ConfigModel(BaseModel):
     unit: str = ""
 
 class Attribute(DeviceAccess):
-    def __init__(self, cfg: ConfigModel):
-        super().__init__(cfg)
-
     """
     Class that implements a default device class that just prints out 
     values (Debugging purpose)
     """
-
     def __init__(self, cfg: ConfigModel):
         super().__init__()
         self._cfg = cfg
@@ -32,17 +28,12 @@ class Attribute(DeviceAccess):
         return self._readback
 
     def set(self, value: float):
-<<<<<<<< HEAD:tests/control/dummy-cs-pyaml/dummy-cs-pyaml/dummy_device.py
-        self._cache = Value(value)
-========
-        print(f"{self._setpoint}: set {value}")
         self._cache = value
->>>>>>>> origin/main:tests/dummy_cs/tango/tango/pyaml/attribute.py
 
     def set_and_wait(self, value: float):
         self.set(value)
 
-    def get(self) -> Value:
+    def get(self) -> float:
         return self._cache
 
     def readback(self) -> Value:
