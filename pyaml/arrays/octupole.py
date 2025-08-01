96e8d20898407c56ed0890a228b17e8e7ee4225b
PONS <pons@esrf.fr>
2025-08-01 15:27:53 +0200

    Added magnet array

---
+++ pyaml/arrays/octupole.py
@@ -0,0 +1,19 @@
+from .array import ArrayModel
+from .array import Array
+from ..lattice.element_holder import ElementHolder,MagnetType
+
+# Define the main class name for this module
+PYAMLCLASS = "Octupole"
+
+class ConfigModel(ArrayModel):...
+
+class Octupole(Array):
+    """
+    Class that implements access to arrays (families)
+    """
+    def __init__(self, cfg: ArrayModel):
+        super().__init__(cfg)
+
+    def fill_array(self,holder:ElementHolder):
+        holder.fill_magnet_array(MagnetType.OCTUPOLE,self._cfg.name,self._cfg.elements)
+
