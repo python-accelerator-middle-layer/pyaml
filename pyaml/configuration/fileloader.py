f1774634de83dd7204cfb285cb61720a8dab4070
guillaumepichon <guillaume.pichon@synchrotron-soleil.fr>
2025-08-05 10:30:33 +0200

    Check of cycles in configuration files.

--- pyaml/configuration/fileloader.py
+++ pyaml/configuration/fileloader.py
@@ -15,43 +15,49 @@ from .. import PyAMLException
 
 logger = logging.getLogger(__name__)
 
-#TODO
-#Implement cycle detection in case of wrong yaml/json link that creates a cycle
+accepted_suffixes = [".yaml", ".yml", ".json"]
 
-def load(filename:str) -> Union[dict,list]:
+def load(filename:str, paths_stack:list=None) -> Union[dict,list]:
     """Load recursively a configuration setup"""
     if filename.endswith(".yaml") or filename.endswith(".yml"):
-        l = YAMLLoader(filename)
+        l = YAMLLoader(filename, paths_stack)
     elif filename.endswith(".json"):
-        l = JSONLoader(filename)
+        l = JSONLoader(filename, paths_stack)
     else:
         raise PyAMLException(f"{filename} File format not supported (only .yaml .yml or .json)")
-    return l.load(filename)
+    return l.load()
+
+# Expand condition
+def hasToExpand(value):
+    return isinstance(value,str) and any(value.endswith(suffix) for suffix in accepted_suffixes)
+
 
 # Loader base class (nested files expansion)
 class Loader:
 
-    def __init__(self, filename:str):
-        self.suffixes = []
+    def __init__(self, filename:str, parent_path_stack:list):
         self.path:Path = get_root_folder() / filename
+        self.files_stack = []
+        if parent_path_stack:
+            if any(self.path.samefile(parent_path) for parent_path in parent_path_stack):
+                raise PyAMLException(f"A cycle has been detected: {parent_path_stack}")
+            self.files_stack.extend(parent_path_stack)
+        self.files_stack.append(self.path)
 
-    # Expand condition
-    def hasToExpand(self,value):
-        return isinstance(value,str) and any(value.endswith(suffix) for suffix in self.suffixes)
 
     # Recursively expand a dict
     def expand_dict(self,d:dict):
         for key, value in d.items():
-            if self.hasToExpand(value):
-                d[key] = load(value)
+            if hasToExpand(value):
+                d[key] = load(value, self.files_stack)
             else:
                 self.expand(value)
 
     # Recursively expand a list
     def expand_list(self,l:list):
         for idx,value in enumerate(l):
-            if self.hasToExpand(value):
-                l[idx] = load(value)
+            if hasToExpand(value):
+                l[idx] = load(value, self.files_stack)
             else:
                 self.expand(value)
 
@@ -65,7 +71,7 @@ class Loader:
 
     # Load a file
     def load(self) -> Union[dict,list]:
-        raise Exception(str(self.path) + ": load() method not implemented")
+        raise PyAMLException(str(self.path) + ": load() method not implemented")
 
 class SafeLineLoader(SafeLoader):
 
@@ -93,22 +99,24 @@ class SafeLineLoader(SafeLoader):
 
 # YAML loader
 class YAMLLoader(Loader):
-    
-    def load(self,fileName:str) -> Union[dict,list]:
-        self.path:Path = get_root_folder() / fileName
-        self.suffixes = [".yaml",".yml"]
+    def __init__(self, filename: str, parent_paths_stack:list):
+        super().__init__(filename, parent_paths_stack)
+
+    def load(self) -> Union[dict,list]:
+        logger.log(logging.DEBUG, f"Loading YAML file '{self.path}'")
         with open(self.path) as file:
             try:
                 return self.expand(yaml.load(file,Loader=SafeLineLoader))
             except yaml.YAMLError as e:
-                raise Exception(self.path + ": " + str(e))
+                raise PyAMLException(str(self.path) + ": " + str(e)) from e
 
 # JSON loader
 class JSONLoader(Loader):
+    def __init__(self, filename: str, parent_paths_stack:list):
+        super().__init__(filename, parent_paths_stack)
 
-    def  load(self,fileName:str) -> Union[dict,list]:
+    def  load(self) -> Union[dict,list]:
         logger.log(logging.DEBUG, f"Loading JSON file '{self.path}'")
-        self.suffixes = [".json"]
         with open(self.path) as file:
             try:
                 return self.expand(json.load(file))
