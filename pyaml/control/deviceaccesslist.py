f0d0daba6dd5f9669e414adc189e16dd44c206f9
PONS <pons@esrf.fr>
2025-08-06 09:21:48 +0200

    Added DeviceAccessList

---
+++ pyaml/control/deviceaccesslist.py
@@ -0,0 +1,45 @@
+from abc import ABCMeta, abstractmethod
+
+import numpy.typing as npt
+import numpy as np
+from .readback_value import Value
+
+class DeviceAccessList(metaclass=ABCMeta):
+    """
+    Abstract class providing access to a list of control system float variable
+    """
+
+    @abstractmethod
+    def name(self) -> list[str]:
+        """Return the names of the variables"""
+        pass
+
+    @abstractmethod
+    def measure_name(self) -> list[str]:
+        """Return the names of the measures"""
+        pass
+
+    @abstractmethod
+    def set(self, value: npt.NDArray[np.float64]):
+        """Write a list of control system device variable (i.e. a power supply currents)"""
+        pass
+
+    @abstractmethod
+    def set_and_wait(self, value: npt.NDArray[np.float64]):
+        """Write a list control system device variable (i.e. a power supply currents)"""
+        pass
+
+    @abstractmethod
+    def get(self) -> npt.NDArray[np.float64]:
+        """Return a list of setpoints of control system device variables"""
+        pass
+
+    @abstractmethod
+    def readback(self) -> npt.NDArray[Value]:
+        """Return the measured variables"""
+        pass
+
+    @abstractmethod
+    def unit(self) -> str:
+        """Return the variable unit"""
+        pass
