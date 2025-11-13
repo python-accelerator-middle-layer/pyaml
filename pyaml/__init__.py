4a9446300c32ab3ffbf69021302add1f23ccbf8b
PONS <pons@esrf.fr>
2025-11-13 14:15:41 +0100

    Updated version to 0.1.0

--- pyaml/__init__.py
+++ pyaml/__init__.py
@@ -8,7 +8,7 @@ pyAML
 __title__ = "pyAML"
 __description__ = "Python Accelerator Middle Layer"
 __url__ = "https://github.com/python-accelerator-middle-layer/pyaml"
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 __author__ = "pyAML collaboration"
 __author_email__ = ""
 
