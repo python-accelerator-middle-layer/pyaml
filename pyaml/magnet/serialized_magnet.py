c13e3a617358c05507a454538c238188e020c032
guillaumepichon <guillaume.pichon@synchrotron-soleil.fr>
2025-11-14 12:06:44 +0100

    Progression on developpement. Starts building individual magnets.

--- pyaml/magnet/serialized_magnet.py
+++ pyaml/magnet/serialized_magnet.py
@@ -2,9 +2,13 @@ import numpy as np
 from numpy import typing as npt
 from pydantic import BaseModel,ConfigDict
 
+from .magnet import Magnet, MagnetConfigModel
 from .model import MagnetModel
+from .. import PyAMLException
 from ..common.element import Element, ElementConfigModel
+from ..configuration import Factory
 from ..control.deviceaccess import DeviceAccess
+from .function_mapping import function_map
 
 # Define the main class name for this module
 PYAMLCLASS = "SerializedMagnetsModel"
@@ -42,16 +46,47 @@ class SerializedMagnetsModel(Element):
         In case of wrong initialization
     """
 
-    def __init__(self, cfg: ConfigModel):
+    def __init__(self, cfg: ConfigModel, peer = None):
         super().__init__(cfg.name)
         self._cfg = cfg
         self.model = cfg.model
+        self.polynoms = []
+        self.__virtuals:list[Magnet] = []
+
+        if peer is None:
+
+            # Configuration part
+            if not self._cfg.function in function_map:
+                raise PyAMLException(self._cfg.function + " not implemented for serialized magnet")
+            for element in self._cfg.elements:
+                # Check mapping validity
+                self.polynoms.append(function_map[self._cfg.function].polynom)
+                # Create the virtual magnet for the corresponding magnet
+                vm = self.__create_virtual_magnet(element)
+                self.__virtuals.append(vm)
+                # Register the virtual element in the factory to have a coherent factory and improve error reporting
+                Factory.register_element(vm)
+        else:
+             # Attach
+             self._peer = peer
+
+    def __create_virtual_magnet(self,name:str) -> Magnet:
+            args = {"name":name,"model":self.model}
+            virtual:Magnet = function_map[self._cfg.function](MagnetConfigModel(**args))
+            virtual.set_model_name(self.get_name())
+            return virtual
 
     def compute_hardware_values(self, strengths: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         pass
 
     def compute_strengths(self, hardware_values: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         pass
+
+    def get_series_name(self) -> str:
+        return self._cfg.name
+
+    def nb_magnets(self) -> int:
+        return len(self._cfg.elements)
 
     def get_strength_units(self) -> list[str]:
         return [self._cfg.unit]*len(self._cfg.elements)
