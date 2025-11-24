Fix sign issue for horizontal kick angle

pyaml/lattice/abstract_impl.py
@@ -28,15 +28,16 @@ class RWHardwareScalar(abstract.ReadWriteFloatScalar):
         self.__model = model
         self.__elements = elements
         self.__poly = elements[0].__getattribute__(poly.attName)
+        self.__sign = poly.sign
         self.__polyIdx = poly.index
 
     def get(self) -> float:
-        s = self.__poly[self.__polyIdx] * self.__elements[0].Length
+        s = self.__poly[self.__polyIdx] * self.__sign * self.__elements[0].Length
         return self.__model.compute_hardware_values([s])[0]
 
     def set(self, value: float):
         s = self.__model.compute_strengths([value])[0]
-        self.__poly[self.__polyIdx] = s / self.__elements[0].Length
+        self.__poly[self.__polyIdx] = s / (self.__elements[0].Length * self.__sign)
 
     def set_and_wait(self, value: float):
         raise NotImplementedError("Not implemented yet.")
@@ -59,15 +60,16 @@ class RWStrengthScalar(abstract.ReadWriteFloatScalar):
         self.__model = model
         self.__elements = elements
         self.__poly = elements[0].__getattribute__(poly.attName)
+        self.__sign = poly.sign
         self.__polyIdx = poly.index
 
     # Gets the value
     def get(self) -> float:
-        return self.__poly[self.__polyIdx] * self.__elements[0].Length
+        return self.__poly[self.__polyIdx] * self.__sign * self.__elements[0].Length
 
     # Sets the value
-    def set(self, value: float):
-        self.__poly[self.__polyIdx] = value / self.__elements[0].Length
+    def set(self, value:float):
+        self.__poly[self.__polyIdx] = value / (self.__elements[0].Length * self.__sign)
 
     # Sets the value and wait that the read value reach the setpoint
     def set_and_wait(self, value: float):
@@ -93,17 +95,19 @@ class RWHardwareArray(abstract.ReadWriteFloatArray):
         self.__elements = elements
         self.__poly = []
         self.__polyIdx = []
+        self.__sign = []
         self.__model = model
         for p in poly:
             self.__poly.append(elements[0].__getattribute__(p.attName))
             self.__polyIdx.append(p.index)
+            self.__sign.append(p.sign)
 
     # Gets the value
     def get(self) -> np.array:
         nbStrength = len(self.__poly)
         s = np.zeros(nbStrength)
         for i in range(nbStrength):
-            s[i] = self.__poly[i][self.__polyIdx[i]] * self.__elements[0].Length
+            s[i] = self.__poly[i][self.__polyIdx[i]] * self.__sign[i] * self.__elements[0].Length
         return self.__model.compute_hardware_values(s)
 
     # Sets the value
@@ -111,7 +115,7 @@ class RWHardwareArray(abstract.ReadWriteFloatArray):
         nbStrength = len(self.__poly)
         s = self.__model.compute_strengths(value)
         for i in range(nbStrength):
-            self.__poly[i][self.__polyIdx[i]] = s[i] / self.__elements[0].Length
+            self.__poly[i][self.__polyIdx[i]] = s[i] / (self.__elements[0].Length * self.__sign[i])
 
     # Sets the value and wait that the read value reach the setpoint
     def set_and_wait(self, value: np.array):
@@ -136,17 +140,19 @@ class RWStrengthArray(abstract.ReadWriteFloatArray):
         self.__elements = elements
         self.__poly = []
         self.__polyIdx = []
+        self.__sign = []
         self.__model = model
         for p in poly:
             self.__poly.append(elements[0].__getattribute__(p.attName))
             self.__polyIdx.append(p.index)
+            self.__sign.append(p.sign)
 
     # Gets the value
     def get(self) -> np.array:
         nbStrength = len(self.__poly)
         s = np.zeros(nbStrength)
         for i in range(nbStrength):
-            s[i] = self.__poly[i][self.__polyIdx[i]] * self.__elements[0].Length
+            s[i] = self.__poly[i][self.__polyIdx[i]] * self.__sign[i] * self.__elements[0].Length
         return s
 
     # Sets the value
@@ -154,7 +160,7 @@ class RWStrengthArray(abstract.ReadWriteFloatArray):
         nbStrength = len(self.__poly)
         s = np.zeros(nbStrength)
         for i in range(nbStrength):
-            self.__poly[i][self.__polyIdx[i]] = value[i] / self.__elements[0].Length
+            self.__poly[i][self.__polyIdx[i]] = value[i] / (self.__elements[0].Length * self.__sign[i])
 
     # Sets the value and wait that the read value reach the setpoint
     def set_and_wait(self, value: np.array):

tests/test_arrays.py
@@ -25,29 +25,53 @@ def test_arrays(install_test_package):
     sr.design.get_magnet("SH1A-C01-H").strength.set(0.000010)
     sr.design.get_magnet("SH1A-C01-V").strength.set(0.000015)
 
+<<<<<<< HEAD
     o, _ = sr.design.get_lattice().find_orbit()
     assert np.abs(o[0] + 9.91848416e-05) < 1e-10
     assert np.abs(o[1] + 3.54829761e-07) < 1e-10
     assert np.abs(o[2] + 1.56246320e-06) < 1e-10
     assert np.abs(o[3] + 1.75037311e-05) < 1e-10
+=======
+    o,_ = sr.design.get_lattice().find_orbit()
+    assert(np.abs(o[0] - 9.90267693e-05)<1e-10)
+    assert(np.abs(o[1] - 3.39661431e-07)<1e-10)
+    assert(np.abs(o[2] + 1.59928207e-06)<1e-10)
+    assert(np.abs(o[3] + 1.74771216e-05)<1e-10)
+>>>>>>> Fix sign issue for horizontal kick angle
 
     sr.design.get_magnet("SH1A-C02-H").strength.set(-0.000008)
     sr.design.get_magnet("SH1A-C02-V").strength.set(-0.000017)
 
+<<<<<<< HEAD
     o, _ = sr.design.get_lattice().find_orbit()
     assert np.abs(o[0] + 1.60277642e-04) < 1e-10
     assert np.abs(o[1] - 2.36103795e-06) < 1e-10
     assert np.abs(o[2] - 3.62843295e-05) < 1e-10
     assert np.abs(o[3] + 6.06571010e-06) < 1e-10
+=======
+    o,_ = sr.design.get_lattice().find_orbit()
+    assert(np.abs(o[0] - 1.60555804e-04)<1e-10)
+    assert(np.abs(o[1] + 2.37234366e-06)<1e-10)
+    assert(np.abs(o[2] - 3.62695844e-05)<1e-10)
+    assert(np.abs(o[3] + 5.97692290e-06)<1e-10)
+>>>>>>> Fix sign issue for horizontal kick angle
 
     sr.design.get_magnets("HCORR").strengths.set([0.000010, -0.000008])
     sr.design.get_magnets("VCORR").strengths.set([0.000015, -0.000017])
 
+<<<<<<< HEAD
     o, _ = sr.design.get_lattice().find_orbit()
     assert np.abs(o[0] + 1.60277642e-04) < 1e-10
     assert np.abs(o[1] - 2.36103795e-06) < 1e-10
     assert np.abs(o[2] - 3.62843295e-05) < 1e-10
     assert np.abs(o[3] + 6.06571010e-06) < 1e-10
+=======
+    o,_ = sr.design.get_lattice().find_orbit()
+    assert(np.abs(o[0] - 1.60555804e-04)<1e-10)
+    assert(np.abs(o[1] + 2.37234366e-06)<1e-10)
+    assert(np.abs(o[2] - 3.62695844e-05)<1e-10)
+    assert(np.abs(o[3] + 5.97692290e-06)<1e-10)
+>>>>>>> Fix sign issue for horizontal kick angle
 
     # Test on control system
 
@@ -118,10 +142,10 @@ def test_arrays(install_test_package):
 
     # Using aggregator
     pos = sr.design.get_bpms("BPMS").positions.get()
-    assert np.abs(pos[0][0] + 7.21154171490481e-05) < 1e-10
-    assert np.abs(pos[0][1] - 3.3988843436571406e-05) < 1e-10
-    assert np.abs(pos[1][0] - 1.1681211772781844e-04) < 1e-10
-    assert np.abs(pos[1][1] - 7.072972488250373e-06) < 1e-10
+    assert(np.abs(pos[0][0] - 7.22262850488348e-05)<1e-10)
+    assert(np.abs(pos[0][1] - 3.4291613955705856e-05)<1e-10)
+    assert(np.abs(pos[1][0] + 1.1696152238807462e-04)<1e-10)
+    assert(np.abs(pos[1][1] - 7.4265634524358045e-06)<1e-10)
 
     # Using aggregator (h and v)
     pos_h = sr.design.get_bpms("BPMS").h.get()
@@ -136,10 +160,17 @@ def test_arrays(install_test_package):
 
     bpms = BPMArray("BPM_noagg", bpms, use_aggregator=False)
     pos = bpms.positions.get()
+<<<<<<< HEAD
     assert np.abs(pos[0][0] + 7.21154171490481e-05) < 1e-10
     assert np.abs(pos[0][1] - 3.3988843436571406e-05) < 1e-10
     assert np.abs(pos[1][0] - 1.1681211772781844e-04) < 1e-10
     assert np.abs(pos[1][1] - 7.072972488250373e-06) < 1e-10
+=======
+    assert(np.abs(pos[0][0] - 7.22262850488348e-05)<1e-10)
+    assert(np.abs(pos[0][1] - 3.4291613955705856e-05)<1e-10)
+    assert(np.abs(pos[1][0] + 1.1696152238807462e-04)<1e-10)
+    assert(np.abs(pos[1][1] - 7.4265634524358045e-06)<1e-10)
+>>>>>>> Fix sign issue for horizontal kick angle
 
     # Radom array
     elts = sr.design.get_elemens("ElArray")

tests/test_ident_models.py
@@ -39,10 +39,10 @@ def test_cfm_magnets(magnet_file, install_test_package):
     magnet_h_design.strength.set(0.000010)
     magnet_v_design.strength.set(0.000015)
 
-    o, _ = sr.design.get_lattice().find_orbit()
-    assert np.abs(o[0] + 9.91848416e-05) < 1e-10
-    assert np.abs(o[1] + 3.54829761e-07) < 1e-10
-    assert np.abs(o[2] + 1.56246320e-06) < 1e-10
-    assert np.abs(o[3] + 1.75037311e-05) < 1e-10
+    o,_ = sr.design.get_lattice().find_orbit()
+    assert(np.abs(o[0] - 9.90267693e-05)<1e-10)
+    assert(np.abs(o[1] - 3.39661431e-07)<1e-10)
+    assert(np.abs(o[2] + 1.59928207e-06)<1e-10)
+    assert(np.abs(o[3] + 1.74771216e-05)<1e-10)
 
     Factory.clear()