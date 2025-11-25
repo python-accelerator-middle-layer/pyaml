a8bc34d7bd57d726609c7a56f561bddfa94a699c
kparasch <konstantinos.paraschou@desy.de>
2025-11-25 17:30:28 +0100

    use parenthesis to split line isntead

--- examples/ESRF_ORM_example/measure_reduced_ORM.py
+++ examples/ESRF_ORM_example/measure_reduced_ORM.py
@@ -14,8 +14,8 @@ orm = OrbitResponseMatrix(cfg=ORM_ConfigModel(bpm_array_name='BPM', hcorr_array_
 
 hcorr = element_holder.get_magnets('HCorr')
 vcorr = element_holder.get_magnets('VCorr')
-corrector_names = hcorr['SJ2A*'].names() + hcorr['SF2A*'].names() + hcorr['SI2A*'].names()\
-                + vcorr['SJ2A*'].names() + vcorr['SF2A*'].names() + vcorr['SI2A*'].names()
+corrector_names = (hcorr['SJ2A*'].names() + hcorr['SF2A*'].names() + hcorr['SI2A*'].names()
+                  + vcorr['SJ2A*'].names() + vcorr['SF2A*'].names() + vcorr['SI2A*'].names())
 
 orm.measure(corrector_names=corrector_names)
 orm.save(parent_folder / Path('reduced_orm.json'))
