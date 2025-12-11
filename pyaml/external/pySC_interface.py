bcff1c682e3a8588a82a0f860316e68e5a25cba7
kparasch <konstantinos.paraschou@desy.de>
2025-12-11 17:57:57 +0100

    remove (default) harcoded names

--- pyaml/external/pySC_interface.py
+++ pyaml/external/pySC_interface.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Dict, List, Tuple
+from typing import TYPE_CHECKING, Tuple, Optional
 
 import numpy as np
 
@@ -11,15 +11,18 @@ class pySCInterface:
     def __init__(
         self,
         element_holder: "ElementHolder",
-        bpm_array_name: str = "BPM",
-        rf_plant_name: str = "RF",
+        bpm_array_name: str,
+        rf_plant_name: Optional[str] = None,
     ):
         self.element_holder = element_holder
 
         self.bpm_array = element_holder.get_bpms(bpm_array_name)
 
         self.rf_plant_name = rf_plant_name
-        self.rf_plant = element_holder.get_rf_plant(self.rf_plant_name)
+        if rf_plant_name is not None:
+            self.rf_plant = element_holder.get_rf_plant(self.rf_plant_name)
+        else:
+            self.rf_plant = None
 
     def get_orbit(self) -> Tuple[np.array, np.array]:
         # we should wait here somehow according to polling rate
@@ -36,8 +39,12 @@ class pySCInterface:
         return
 
     def get_rf_main_frequency(self) -> float:
+        if self.rf_plant is None:
+            raise PyAMLException("RF plant name was not provided.")
         return self.rf_plant.frequency.get()
 
     def set_rf_main_frequency(self, value: float) -> None:
+        if self.rf_plant is None:
+            raise PyAMLException("RF plant name was not provided.")
         self.rf_plant.frequency.set(value)
         return
