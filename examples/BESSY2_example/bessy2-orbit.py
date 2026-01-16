44050b081ceb5da3ca2bede43993575eeca39034
PONS <pons@esrf.fr>
2026-01-16 10:44:19 +0100

    Added tune

--- examples/BESSY2_example/bessy2-orbit.py
+++ examples/BESSY2_example/bessy2-orbit.py
@@ -1,8 +1,9 @@
-import numpy as np
 import time
 
-from pyaml.accelerator import Accelerator
 import matplotlib.pyplot as plt
+import numpy as np
+
+from pyaml.accelerator import Accelerator
 
 sr = Accelerator.load("BESSY2Orbit.yaml")
 orbit = sr.live.get_bpms("BPM").positions
@@ -22,8 +23,8 @@ ax2 = fig.add_subplot(312)
 ax3 = fig.add_subplot(313)
 ax1.plot(positions_bc[:, 0] * 1e-3, label="Orbit before correction")
 ax2.plot(positions_bc[:, 1] * 1e-3, label="Orbit before correction")
-#ax1.plot(positions_ac[:, 0] * 1e6, label="Orbit after correction")
-#ax2.plot(positions_ac[:, 1] * 1e6, label="Orbit after correction")
+# ax1.plot(positions_ac[:, 0] * 1e6, label="Orbit after correction")
+# ax2.plot(positions_ac[:, 1] * 1e6, label="Orbit after correction")
 
 ax3.plot(hcorr.strengths.get())
 ax3.plot(vcorr.strengths.get())
@@ -36,4 +37,3 @@ ax3.set_xlabel("Steerer number")
 fig.tight_layout()
 
 plt.show()
-
