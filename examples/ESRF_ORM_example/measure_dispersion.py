ff8ce885f848fb37c85005d040f9d23eeaf8a4ec
kparasch <konstantinos.paraschou@desy.de>
2026-01-28 09:01:01 +0100

    adapt dispersion example

--- examples/ESRF_ORM_example/measure_dispersion.py
+++ examples/ESRF_ORM_example/measure_dispersion.py
@@ -11,19 +11,10 @@ config_path = parent_folder.parent.parent.joinpath(
     "tests", "config", "EBSOrbit.yaml"
 ).resolve()
 sr = Accelerator.load(config_path)
-element_holder = sr.design
+eh = sr.design
 
-dispersion = Dispersion(
-    cfg=Disp_ConfigModel(
-        bpm_array_name="BPM",
-        rf_plant_name="RF",
-        frequency_delta=10,
-    ),
-    element_holder=element_holder,
-)
-
-dispersion.measure()
-dispersion_data = dispersion.get()
+eh.dispersion.measure()
+dispersion_data = eh.dispersion.get()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
