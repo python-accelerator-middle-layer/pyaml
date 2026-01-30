70ede62dad0af4f32374004e8c06df696bd9a90f
liuzzo <simone.liuzzo@esrf.fr>
2026-01-30 09:08:53 +0100

    modified tune example for esrf

--- examples/ESRF_tune_example/esrf_tune_example.py
+++ examples/ESRF_tune_example/esrf_tune_example.py
@@ -6,17 +6,12 @@ from pyaml.accelerator import Accelerator
 from pyaml.common.constants import ACTION_RESTORE
 from pyaml.magnet.magnet import Magnet
 
-# Get the directory of the current script
-script_dir = os.path.dirname(__file__)
-
-# Go up one level and then into 'data'
-relative_path = os.path.join(script_dir, "..", "..", "tests", "config", "EBSTune.yaml")
-
-# Normalize the path (resolves '..')
-absolute_path = os.path.abspath(relative_path)
-
-sr: Accelerator = Accelerator.load(absolute_path)
+sr: Accelerator = Accelerator.load('./pyaml/tests/config/EBSTune.yaml')
 sr.design.get_lattice().disable_6d()
+
+# switch script from live to design
+# SR = sr.live   # act on live machine
+SR = sr.design # act on simulation 
 
 
 # Callback exectued after each magnet strenght setting
@@ -33,9 +28,9 @@ tune_adjust_design = sr.design.get_tune_tuning("TUNE")
 tune_adjust_design.response.measure(callback=tune_callback)
 tune_adjust_design.response.save_json("tunemat.json")
 
-# Correct tune on live
-tune_adjust_live = sr.live.get_tune_tuning("TUNE")
-tune_adjust_live.response.load_json("tunemat.json")
-print(tune_adjust_live.readback())
-tune_adjust_live.set([0.17, 0.32], iter=2, wait_time=10)
-print(tune_adjust_live.readback())
+# Correct tune on live or design
+tune_adjust = SR.get_tune_tuning("TUNE")
+tune_adjust.response.load_json("tunemat.json")
+print(tune_adjust.readback())
+tune_adjust.set([0.17, 0.32], iter=2, wait_time=10)
+print(tune_adjust.readback())
