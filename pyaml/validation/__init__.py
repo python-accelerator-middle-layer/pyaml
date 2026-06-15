aebe9f8237e2f96a4b3d7d7d0f4e7d3904b48f0d
Teresia Olsson <teresia.olsson@helmholtz-berlin.de>
2026-06-15 15:05:27 +0200

    Added validation at object creation.

--- pyaml/validation/__init__.py
+++ pyaml/validation/__init__.py
@@ -3,14 +3,16 @@ PyAML validation subpackage.
 """
 
 from .generator import SchemaGenerator
-from .models import ConfigurationSchema
+from .models import ConfigurationSchema, DynamicValidation, StaticValidation
 from .registry import SchemaRegistry, register_schema
 from .validator import SchemaValidator
 
 __all__ = [
     "ConfigurationSchema",
+    "DynamicValidation",
     "SchemaRegistry",
     "SchemaValidator",
     "SchemaGenerator",
+    "StaticValidation",
     "register_schema",
 ]
